73593424f912a8824e9ab2a8abde852619f406c8
Melih Bektaş <melihbektas92@gmail.com>
2021-06-29 20:07:16 +0300

    Missle System Added

--- Tower Defence/Assets/Scripts/Bullet.cs
+++ Tower Defence/Assets/Scripts/Bullet.cs
@@ -10,6 +10,8 @@ public class Bullet : MonoBehaviour
 
     public float speed = 70f;
 
+    public float explosionRadius = 0f;
+
     public void Seek(Transform _target)
     {
         target = _target;
@@ -35,7 +37,7 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-
+        transform.LookAt(target);
 
 
     }
@@ -43,10 +45,41 @@ public class Bullet : MonoBehaviour
     private void HitTarget()
     {
         GameObject effectIns =(GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
+        Destroy(effectIns, 5f);
 
-        Destroy(target.gameObject);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
 
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(target.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
